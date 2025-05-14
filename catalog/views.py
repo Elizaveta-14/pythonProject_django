e3488129f68cfde88742d9cfbd6366a313e6b9e6
Elizaveta <you@example.com>
2025-05-15 00:04:21 +0300

    add homework24

--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,36 @@
-
 # Create your views here.
-from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.shortcuts import render , get_object_or_404
+
+from catalog.models import Product
 
 
 def home(requests):
-    return render(requests, template_name='catalog/home.html')
+    return render(requests, template_name="catalog/home.html")
 
 
 def contacts(requests):
-    return render(requests, template_name='catalog/contacts.html')
+    return render(requests, template_name="catalog/contacts.html")
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+
+        return HttpResponse(f"Спасибо {name}! Сообщение получено")
+    return render(request, "contacts.html")
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "products_list.html", context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'products_detail.html', context)
